358e033dbe25db29d46d77096b20b6d37ca02dff
Ron <rc72095@icloud.com>
2023-10-08 09:56:53 -0400

    add routing and install firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import Directory from './ components/directory/directory.component';
+import { Routes, Route, Outlet} from 'react-router-dom'
+import Navigation from './routes/navigation/navigation.component'
+import Home from './routes/home/home.component'
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 
 const App = () => {
+ 
   return (
-    <Directory />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/> 
+        <Route path='/shop' element={<Shop />} />
+      </Route>
+    </Routes>
+    
   );
 }
 
