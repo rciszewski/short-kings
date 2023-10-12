9d5c0668a489103bad16930853d8b253db406085
Ron <rc72095@icloud.com>
2023-10-11 20:25:59 -0400

    added sign in component, firebase utils, and brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
   signInWithPopup, 
   GoogleAuthProvider,
 } from 'firebase/auth'
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwuKEauF1qXQ22XH6KDx4fD3NmOa4Ajn4",
@@ -26,3 +32,29 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid )
+
+  const userSnapshot = await getDoc(userDocRef)
+
+  // Check to see if snapshop exists
+  if(!userSnapshot.exists()){
+    const { displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email, 
+        createdAt,
+      })
+    } catch (error){
+      console.log('error in createUserDocumentFromAuth', error)
+    }    
+  }
+
+  return userDocRef;
+}
