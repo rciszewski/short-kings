cb4b307d08211607d6e89405683a3af9b9b00553
Ron <rc72095@icloud.com>
2023-11-12 18:07:25 -0500

    add styles to product cards and setup product card container

--- src/ components/product-card/product-card.component.jsx
+++ src/ components/product-card/product-card.component.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./product-card.styles.scss";
+import Button from "../button/button.component";
 
-const ProductCard = ({product}) => {
-  // const { name, imageUrl, price} = product;
-  console.log(product);
+const ProductCard = ({ product }) => {
+  const { name, imageUrl, price } = product;
   return (
-    <div className="product-card">
-      <img src={product.imageUrl} alt={`${product.name} product card`} />
-      <div className="product-name-and-price">
-        <span>{product.name}</span>
-        <span>{product.price}</span>
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name} product card`} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
+      <Button buttonType="inverted">Add to cart</Button>
     </div>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { useContext } from "react";
 import { ProductsContext } from "../../contexts/products.context";
 import ProductCard from "../../ components/product-card/product-card.component";
+import "./shop.component.styles.scss";
 
 const Shop = () => {
   const { products, setProducts } = useContext(ProductsContext);
   return (
-    <>
+    <div className="products-container">
       {products.map((product) => {
         return <ProductCard key={product.id} product={product} />;
       })}
-    </>
+    </div>
   );
 };
 
